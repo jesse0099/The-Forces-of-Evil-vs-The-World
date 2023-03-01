c00840f833f426d7fdedf3cc5089cf3865687b42
PabloJPS <pabloporrasjs@gmail.com>
2023-02-28 20:57:30 -0600

    HUD Pablo Finalizada con Jesé

--- Assets/Scripts/player/PlayerStats.cs
+++ Assets/Scripts/player/PlayerStats.cs
@@ -9,6 +9,15 @@ public class PlayerStats : MonoBehaviour
     public int player_mana = 50;
     public int damage;
     Animator animator;
+
+    public HealthBar healthBar;
+    public int maxHealth = 100;
+    public ManaBar manaBar;
+    public int maxMana = 100;
+
+    public Points score;
+    public int actualPoints = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +27,115 @@ public class PlayerStats : MonoBehaviour
         animator = GetComponent<Animator>();
         Console.WriteLine("Current Heath: " + player_health);
         Console.WriteLine("Current Mana: " + player_mana);
+
+        player_health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        player_mana = maxMana;
+        manaBar.SetMaxMana(maxMana);
+
+        score.SetPoints(actualPoints);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+            SpendMana(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SumPoints(20);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SubPoints(40);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        player_health -= damage;
+        if (player_health <= 0)
+        {
+            healthBar.SetHealth(0);
+            player_health = 0;
+        }
+        else
+        {
+            healthBar.SetHealth(player_health);
+        }
+
+    }
+
+    void Heal(int heal)
+    {
+        player_health += heal;
+
+        if (player_health >= maxHealth)
+        {
+            healthBar.SetHealth(maxHealth);
+            player_health = maxHealth;
+        }
+        else
+        {
+            healthBar.SetHealth(player_health);
+        }
+    }
+
+    void SpendMana(int mana)
+    {
+        player_mana -= mana;
+
+        if (player_mana <= 0)
+        {
+            manaBar.SetMana(0);
+            player_mana = 0;
+        }
+        else
+        {
+            manaBar.SetMana(player_mana);
+        }
+    }
+
+    void RecoverMana(int mana)
+    {
+        player_mana += mana;
+
+        if (player_mana >= maxMana)
+        {
+            manaBar.SetMana(maxMana);
+            player_mana = maxMana;
+        }
+        else
+        {
+            manaBar.SetMana(player_mana);
+        }
+    }
+
+    void SumPoints(int points)
+    {
+        actualPoints += points;
+        score.SetPoints(actualPoints);
+    }
+
+    void SubPoints(int points)
+    {
+        actualPoints -= points;
+
+        if (actualPoints <= 0)
+        {
+            score.SetPoints(0);
+            actualPoints = 0;
+        }
+        else
+        {
+            score.SetPoints(actualPoints);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,7 +143,7 @@ public class PlayerStats : MonoBehaviour
         if (collision.gameObject.CompareTag("potion"))
         {
             Console.WriteLine("Player Restored Health");
-            player_health= 100;
+            Heal(40);
             Destroy(collision.gameObject);
             Console.WriteLine("Current Heath: " + player_health);
             Console.WriteLine("Current Mana: " + player_mana);
@@ -39,7 +151,7 @@ public class PlayerStats : MonoBehaviour
         if (collision.gameObject.CompareTag("mana"))
         {
             Console.WriteLine("Player Restored Mana");
-            player_mana= 100;
+            RecoverMana(50);
             Destroy(collision.gameObject);
             Console.WriteLine("Current Heath: " + player_health);
             Console.WriteLine("Current Mana: " + player_mana);
